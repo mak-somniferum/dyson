bc145be1f4acbbfd0389edae7c514e3a733d0980
ByeongKyu-Kim <tjsanfvh12@naver.com>
2020-04-27 18:08:03 +0900

    customer  서브 페이지 작성
    customer  서브 페이지 작성

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,21 @@
 // header, footer LOAD
 $('#header').load('header.html')
 $('#footer').load('footer.html')
+
+//customer
+$('.check_box li').click(function(){
+    $('.check_box').fadeOut()
+    $('.customer_box').fadeIn()
+    var idx = $(this).index()
+    $('.check_text li').removeClass('on')
+    $('.check_text li').eq(idx).addClass('on')
+    $('.part_box li').fadeOut()
+    $('.part_box li').eq(idx).fadeIn()
+})
+$('.check_text li').click(function(){
+    var idx = $(this).index()
+    $('.check_text li').removeClass('on')
+    $(this).addClass('on')
+    $('.part_box li').fadeOut()
+    $('.part_box li').eq(idx).fadeIn()
+})
