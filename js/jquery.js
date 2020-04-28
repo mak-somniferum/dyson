0ca2711e18a63deabe10d4b1f307efc50bca43bf
seongwook <to_ihnen@naver.com>
2020-04-28 18:24:41 +0900

    박성욱 18:23 마지막
     마지막 등록

--- js/jquery.js
+++ js/jquery.js
@@ -19,3 +19,43 @@ $('.check_text li').click(function(){
     $('.part_box li').fadeOut()
     $('.part_box li').eq(idx).fadeIn()
 })
+
+
+
+
+
+// promotion      // promotion      // promotion
+// promotion      // promotion      // promotion
+// promotion      // promotion      // promotion
+
+
+$(".s_arti1 > div").click(function(){
+    $(".s_arti1 > div").removeClass("s_oo")
+        $(this).addClass("s_oo")
+})
+
+$(".s_arti1 > div > div").click(function(){
+    $(".s_arti1 > div > div").removeClass("s_pp")
+        $(this).addClass("s_pp")
+})
+
+
+$(".s_arti1 > div > div").click(function(){
+    $(".s_arti1 > div > div").removeClass("s_kk")
+        $(this).addClass("s_kk")
+})
+
+
+var swiper = new Swiper('.s_arti .swiper-container', {
+    effect: 'cube',
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: '.s_arti .swiper-pagination',
+    },
+  });
