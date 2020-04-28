3d3ec0329d348474f2586d0671bdcf309c2ca504
ByeongKyu-Kim <tjsanfvh12@naver.com>
2020-04-28 17:04:35 +0900

    customer 부분 내용 추가건
    customer 부분 내용 추가건

--- js/jquery.js
+++ js/jquery.js
@@ -2,20 +2,27 @@
 $('#header').load('header.html')
 $('#footer').load('footer.html')
 
+//===============================================================================
 //customer
-$('.check_box li').click(function(){
-    $('.check_box').fadeOut()
+$('.cust_check_box li').click(function(){
+    $('.cust_check_box').fadeOut()
     $('.customer_box').fadeIn()
     var idx = $(this).index()
-    $('.check_text li').removeClass('on')
-    $('.check_text li').eq(idx).addClass('on')
-    $('.part_box li').fadeOut()
-    $('.part_box li').eq(idx).fadeIn()
+    $('.cust_check_text li').removeClass('on')
+    $('.cust_check_text li').eq(idx).addClass('on')
+    $('.cust_part_box li').fadeOut()
+    $('.cust_part_box li').eq(idx).fadeIn()
 })
-$('.check_text li').click(function(){
+$('.cust_check_box li').mouseenter(function(){
+    $(this).find('img').animate({'width': '70%'})
+})
+$('.cust_check_box li').mouseleave(function(){
+    $('.cust_check_box img').animate({'width': '60%'})
+})
+$('.cust_check_text li').click(function(){
     var idx = $(this).index()
-    $('.check_text li').removeClass('on')
+    $('.cust_check_text li').removeClass('on')
     $(this).addClass('on')
-    $('.part_box li').fadeOut()
-    $('.part_box li').eq(idx).fadeIn()
+    $('.cust_part_box li').fadeOut()
+    $('.cust_part_box li').eq(idx).fadeIn()
 })
