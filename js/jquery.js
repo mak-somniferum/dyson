8c82423abde5f3582808a6dc2085edbcc1f00f28
shinjuyeon <3460shin@naver.com>
2020-04-28 18:23:21 +0900

    0428 서브페이지 수정중

--- js/jquery.js
+++ js/jquery.js
@@ -2,6 +2,17 @@
 $('#header').load('header.html')
 $('#footer').load('footer.html')
 
+//==================================== register
+
+$('.regi_bg_img').first().fadeOut(2000)
+$('.regi_bg_img').last().fadeIn(2000)
+
+$('.regi_bg_m').animate({'height':'50%'},2000)
+
+
+//==================================== register end
+
+
 //customer
 $('.check_box li').click(function(){
     $('.check_box').fadeOut()
@@ -19,3 +30,38 @@ $('.check_text li').click(function(){
     $('.part_box li').fadeOut()
     $('.part_box li').eq(idx).fadeIn()
 })
+
+
+
+
+
+//===============================responseible
+
+windowReposive()
+$(window).resize(function(){
+    windowReposive()
+})
+function windowReposive(){
+    var windowWidth=$(window).width()
+
+    if(windowWidth >= 1200){
+        
+        $('.regi_txt').animate({'opacity': '1', top : '40%'},2000)
+        $('.regi_prod').animate({bottom : '5%'},2000)
+
+
+    }else if(windowWidth <= 1200){
+        
+        $('.regi_txt').animate({'opacity': '1', top : '75%'},2000)
+        $('.regi_prod').animate({bottom : '10%'},2000)
+
+
+    }
+    else if(windowWidth <= 768){
+        
+        $('.regi_txt').animate({'opacity': '1', top : '75%'},2000)
+        $('.regi_prod').animate({bottom : '-10%'},2000)
+
+
+    }
+}
